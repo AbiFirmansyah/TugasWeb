ea007dd688e5ef6f40ee82e716af3d051dbd55e3
AbiFirmansyah <93362335+AbiFirmansyah@users.noreply.github.com>
2021-11-27 13:48:56 +0700

    Add files via upload

---
+++ Main.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Tugas</title>
+    <link rel="stylesheet" href="editor.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="header-left">
+          <img class="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Code.svg/1200px-Code.svg.png">
+        </div>
+        <div class="header-right">
+          <a href="Form Login.php" class="login">Log in</a>
+        </div>
+      </div>
+    </header>
+    <div class="top-wrapper">
+      <div class="container">
+        <h1>BELAJAR CODING.</h1>
+        <h1>BELAJAR MENJADI LEBIH KREATIF.</h1>
+        <p>Dibuat tugas pemrogrman web praktik</p>
+        <p>jadi maaf kalo misal halaman website kurang menarik</p>
+        <div class="btn-wrapper">
+          <a href="#" class="btn signup">Daftar dengan Email</a>
+          <p class="edit-p">atau</p>
+          <a href="#" class="btn facebook"><span class="fa fa-facebook"></span>Daftar dengan Facebook</a>
+          <a href="#" class="btn twitter"><span class="fa fa-twitter"></span>Daftar dengan Twitter</a>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
